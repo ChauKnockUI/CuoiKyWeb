0297da13c7aef634c4bc76b8d9e3e9f1a17b746b
unknown <hohaichau963gmail.com>
2023-06-20 16:49:01 +0700

    Fix bug phan Client

---
+++ assets/js/btn-js.js
@@ -0,0 +1,12 @@
+// Payment Settings
+const productButton = document.querySelector(".product-btn");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
